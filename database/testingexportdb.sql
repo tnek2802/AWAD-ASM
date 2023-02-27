d49cfbdd3a71c7f7f64b24b447625a7ab94bd61d
yenkhai8 <limyenjhai01@gmail.com>
2023-02-27 17:24:03 +0800

    yes

---
+++ database/testingexportdb.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 27, 2023 at 09:20 AM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `abibastest`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `testingexportdb`
+--
+
+DROP TABLE IF EXISTS `testingexportdb`;
+CREATE TABLE IF NOT EXISTS `testingexportdb` (
+  `test` int(11) NOT NULL AUTO_INCREMENT,
+  `test2` int(11) NOT NULL,
+  `test3` int(11) NOT NULL,
+  `test4` int(11) NOT NULL,
+  PRIMARY KEY (`test`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
