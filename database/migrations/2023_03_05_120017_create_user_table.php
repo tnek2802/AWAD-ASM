e8881c83d81874da8d560908f1d1c9fd6e138552
jasperyin1234 <jasperyin1234@gmail.com>
2023-03-06 13:11:40 +0800

    Created seeder & All column types specified

---
+++ database/migrations/2023_03_05_120017_create_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->bigIncrements('user_id');
+            $table->string('username', 50);
+            $table->string('password', 60);
+            $table->string('email', 50);
+            $table->char('user_type', 1);
+            $table->integer('contact_num', 11);
+            $table->string('address', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user');
+    }
+}
