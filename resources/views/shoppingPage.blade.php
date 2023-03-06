08aa76d9bf4716b751a9a737456e0fedb4bd6a62
woody047 <88617176+woody047@users.noreply.github.com>
2023-03-06 13:19:40 +0800

    structure

---
+++ resources/views/shoppingPage.blade.php
@@ -0,0 +1,94 @@
+<html>
+<head>
+    <link rel="stylesheet" href="{{url('css/shoppingCart.css')}}"/>
+</head>
+<body>
+
+<section class="section1">
+</section>
+
+<section class="section2">
+    <div class="product">
+
+        <div class="card">
+            <div class="img"><img src="{{url('/images/1.png')}}"></div>
+            <div class="title">New Balance 574</div>
+            <div class="desc">Grade-School Shoes | Reflection 073</div>
+            <div class="box">
+                <div class="price">RM 379.00</div>
+                <button class="btn">Add To Cart</button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="img"><img src="{{url('/images/2.png')}}"></div>
+            <div class="title">Nike E-Series</div>
+            <div class="desc">Grade-School Shoes | Grey-Cosmic</div>
+            <div class="box">
+                <div class="price">RM 488.00</div>
+                <button class="btn">Add To Cart</button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="img"><img src="{{url('/images/3.png')}}"></div>
+            <div class="title">Nike Dunk Low</div>
+            <div class="desc">Men Shoes | Silver-Mint</div>
+            <div class="box">
+                <div class="price">RM 539.00</div>
+                <button class="btn">Add To Cart</button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="img"><img src="{{url('/images/4.png')}}"></div>
+            <div class="title">New Balance Neo</div>
+            <div class="desc">Men Shoes | Sea Salt 108</div>
+            <div class="box">
+                <div class="price">RM 699.00</div>
+                <button class="btn">Add To Cart</button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="img"><img src="{{url('/images/5.png')}}"></div>
+            <div class="title">Nike Air Force 3</div>
+            <div class="desc">Men Shoes | White-In-Blue</div>
+            <div class="box">
+                <div class="price">RM 450.00</div>
+                <button class="btn">Add To Cart</button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="img"><img src="{{url('/images/6.png')}}"></div>
+            <div class="title">Nike Air Trainer</div>
+            <div class="desc">Men Shoes | Black-Dk-Grey</div>
+            <div class="box">
+                <div class="price">RM 589.00</div>
+                <button class="btn">Add To Cart</button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="img"><img src="{{url('/images/7.png')}}"></div>
+            <div class="title">Nike Lebron XX</div>
+            <div class="desc">Men Shoes | Multi-Colour</div>
+            <div class="box">
+                <div class="price">RM 919.00</div>
+                <button class="btn">Add To Cart</button>
+            </div>
+        </div>
+        <div class="card">
+            <div class="img"><img src="{{url('/images/8.png')}}"></div>
+            <div class="title">Nike Dunk Higg</div>
+            <div class="desc">Men Shoes | Chill-Blue</div>
+            <div class="box">
+                <div class="price">RM 425.00</div>
+                <button class="btn">Add To Cart</button>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<!-- <div class="preview">
+    <div class="pre-img active"><img src="{{url('/images/1.png')}}"></div>
+</div> -->
+
+</body>
+</html>
