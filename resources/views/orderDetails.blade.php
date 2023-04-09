727b1e079b997637bc564a9b95d44da1b1df10f3
YenKhai <intern2@dsm.com.my>
2023-04-09 19:43:35 +0800

    order details settle

--- resources/views/orderDetails.blade.php
+++ resources/views/orderDetails.blade.php
@@ -8,48 +8,61 @@
 
     </html>
 
-    <div class=test>
+    {{-- <div class=test>
         <h1>hey there here are your orders xd</h1>
-        {{-- @foreach($orders as $order)
-        <div>{{$order->transaction_id}}</div>
-        @endforeach --}}
-
-        {{-- @foreach ($products as $product)
-        <div class="col-md-3">
-            <div class="card">
-                <div class="card-header">Order ID:{{ $product->transaction_id}}</div>
-                <div class="card-header">{{ $product->product_name }}</div>
+        @foreach ($orders as $order)
+            <div class="card mb-3">
+                <div class="card-header">
+                    Order ID: {{ $order->transaction_id }}
+                </div>
                 <div class="card-body">
-                    <img src="{{ $product->image }}" class='img-fluid'>
-                    <p>{{ $product->product_description }}</p>
-                    <p>Price: {{ $product->product_price }}</p>
+                    @foreach ($order->Products as $product)
+                        <div class="media mb-3">
+                            <img src="{{ $product->image }}" class="mr-3" alt="{{ $product->product_name }}" width="64">
+                            <div class="media-body">
+                                <h5 class="mt-0">{{ $product->product_name }}</h5>
+                                <p>{{ $product->product_description }}</p>
+                                <p>Price: {{ $product->product_price }}</p>
+                                <p>Quantity: {{ $product->pivot->transaction_quantity }}</p>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="card-footer">
+                    <strong>Total:</strong> {{ $order->total_amount }}
                 </div>
             </div>
-        </div>
-        @endforeach --}}
+        @endforeach
+    </div> --}}
 
+    <div class="container">
+        <h1>Here are your orders:</h1>
         @foreach ($orders as $order)
-    <div class="card mb-3">
-        <div class="card-header">
-            Order ID: {{ $order->transaction_id }}
-        </div>
-        <div class="card-body">
-            @foreach ($order->Products as $product)
-                <div class="media mb-3">
-                    <img src="{{ $product->image }}" class="mr-3" alt="{{ $product->product_name }}" width="64">
-                    <div class="media-body">
-                        <h5 class="mt-0">{{ $product->product_name }}</h5>
-                        <p>{{ $product->product_description }}</p>
-                        <p>Price: {{ $product->product_price }}</p>
-                        <p>Quantity: {{ $product->pivot->transaction_quantity }}</p>
-                    </div>
+            <div class="card mb-3">
+                <div class="card-header">
+                    Order ID: {{ $order->transaction_id }}
                 </div>
-            @endforeach
-        </div>
-        <div class="card-footer">
-            <strong>Total:</strong> {{ $order->total_amount }}
-        </div>
+                <div class="card-body row">
+                    @foreach ($order->Products as $product)
+                        <div class="col-md-3">
+                            <div class="card">
+                                <img src="{{ $product->image }}" class="card-img-top" alt="{{ $product->product_name }}">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $product->product_name }}</h5>
+                                    <p class="card-text">{{ $product->product_description }}</p>
+                                    <p class="card-text">Price: {{ $product->product_price }}</p>
+                                    <p class="card-text">Quantity: {{ $product->pivot->transaction_quantity }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="card-footer">
+                    <strong>Total:</strong> {{ $order->total_amount }}
+                </div>
+            </div>
+        @endforeach
     </div>
-@endforeach
-    </div>
+    
+    
 @endsection
