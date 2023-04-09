64d3735ccc42e6d30e86ec1ee951b31f512f68ab
woody047 <88617176+woody047@users.noreply.github.com>
2023-04-09 16:40:33 +0800

    Woody-navigation bar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ __('Abibas') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -18,15 +19,18 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ URL::asset('/css/topnav.css') }}">
+
 </head>
+
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -37,77 +41,74 @@
                     </ul>
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
-                                <li class="nav-item">
+                                <div class="topnav">
+                                    <div class="logo">
+                                        <a href="{{ url('/') }}">
+                                            {{ __('Abibas') }}
+                                        </a>
+                                    </div>
+                                    <a class="nav-link" href='/Cart'>{{ __('Cart') }}</a>
                                     <a class="nav-link" href='/MenClothes'>{{ __('Men Clothes') }}</a>
-                                </li>
-                                <li class="nav-item">
                                     <a class="nav-link" href='/MenShoes'>{{ __('Men Shoes') }}</a>
-                                </li>
-                                <li class="nav-item">
                                     <a class="nav-link" href='/WomenClothes'>{{ __('Women Clothes') }}</a>
-                                </li>
-                                <li class="nav-item">
                                     <a class="nav-link" href='/WomenShoes'>{{ __('Women Shoes') }}</a>
-                                </li>
-                                <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
+                                
                             @endif
                             @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             @endif
-
+                                </div>
                         @else
-                        <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
-                            @if (Route::has('logout'))
-                                <li class="nav-item">
+                            <ul class="navbar-nav ms-auto">
+                                <!-- Authentication Links -->
+                                @if (Route::has('logout'))
+                                    <div class="topnav">
+                                    <div class="logo">
+                                        <a href="{{ url('/') }}">
+                                            {{ __('Abibas') }}
+                                        </a>
+                                    </div>
+                                    <a class="nav-link" href='/Cart'>{{ __('Cart') }}</a>
                                     <a class="nav-link" href='/MenClothes'>{{ __('Men Clothes') }}</a>
-                                </li>
-                                <li class="nav-item">
                                     <a class="nav-link" href='/MenShoes'>{{ __('Men Shoes') }}</a>
-                                </li>
-                                <li class="nav-item">
                                     <a class="nav-link" href='/WomenClothes'>{{ __('Women Clothes') }}</a>
-                                </li>
-                                <li class="nav-item">
                                     <a class="nav-link" href='/WomenShoes'>{{ __('Women Shoes') }}</a>
-                                </li>
-                            @endif
+                                @endif
 
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    Hi, {{ Auth::guard('web')->user()->username }}
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                        onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        Hi, {{ Auth::guard('web')->user()->username }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        </ul>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+                                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                        document.getElementById('logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                    </div>
+                @endguest
+                </ul>
+            </div>
+    </div>
+    </nav>
+
+    <main class="py-4">
+        @yield('content')
+    </main>
     </div>
 </body>
+
 </html>
