e5f618615ec84cacb52f474c586b9135ccd50a75
yenkhai8 <limyenjhai01@gmail.com>
2023-04-09 13:59:08 +0800

    done, left linkage to product

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,24 +8,10 @@ use App\Models\Product;
 class HomeController extends Controller
 {
     /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Show the application dashboard.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    // public function index()
-    // {
-    //     return view('home');
-    // }
 
     public function index()
     {
