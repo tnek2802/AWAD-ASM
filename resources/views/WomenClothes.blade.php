5b1855bfbb29fff9e6f6907b5dd312249f2a76b9
jasperyin1234 <jasperyin1234@gmail.com>
2023-04-09 17:14:45 +0800

    Done product pages output with db data

--- resources/views/WomenClothes.blade.php
+++ resources/views/WomenClothes.blade.php
@@ -8,79 +8,24 @@
 
 <section class="section2">
     <div class="product">
-
-        <div class="card" data-name="p-1">
-            <div class="img"><img src="{{url('/images/w1.png')}}"></div>
-            <div class="title">IVY Park</div>
-            <div class="desc">Sport Bra</div>
+    @foreach ($products as $product)
+        <div class="card" data-name= "{{$product->product_name}}" >
+            <div class="img"><img src="{{ $product->image }}"></div> 
+            <div class="title"> {{$product->product_name}} </div>
+            <div class="desc">{{$product->product_description}}</div>
             <div class="box">
-                <div class="price">RM 79.00</div>
+                <div class="price">RM {{$product->product_price}}</div>
             </div>
         </div>
-        <div class="card" data-name="p-2">
-            <div class="img"><img src="{{url('/images/w2.png')}}"></div>
-            <div class="title">Nike Suit</div>
-            <div class="desc">Hoddie</div>
-            <div class="box">
-                <div class="price">RM 158.00</div>
-            </div>
-        </div>
-        <div class="card" data-name="p-3">
-            <div class="img"><img src="{{url('/images/w3.png')}}"></div>
-            <div class="title">Under Armour Soften</div>
-            <div class="desc">Sport Bra</div>
-            <div class="box">
-                <div class="price">RM 89.00</div>
-            </div>
-        </div>
-        <div class="card" data-name="p-4">
-            <div class="img"><img src="{{url('/images/w4.png')}}"></div>
-            <div class="title">Vogue Flora</div>
-            <div class="desc">Shirt</div>
-            <div class="box">
-                <div class="price">RM 69.00</div>
-            </div>
-        </div>
-        <div class="card" data-name="p-5">
-            <div class="img"><img src="{{url('/images/w5.png')}}"></div>
-            <div class="title">Kappa Style</div>
-            <div class="desc">T-Shirt</div>
-            <div class="box">
-                <div class="price">RM 50.00</div>
-            </div>
-        </div>
-        <div class="card" data-name="p-6">
-            <div class="img"><img src="{{url('/images/w6.png')}}"></div>
-            <div class="title">Abibas Branded</div>
-            <div class="desc">Shorts</div>
-            <div class="box">
-                <div class="price">RM 89.00</div>
-            </div>
-        </div>
-        <div class="card" data-name="p-7">
-            <div class="img"><img src="{{url('/images/w7.png')}}"></div>
-            <div class="title">Relax abibas</div>
-            <div class="desc">Short Pant</div>
-            <div class="box">
-                <div class="price">RM 79.00</div>
-            </div>
-        </div>
-        <div class="card" data-name="p-8">
-            <div class="img"><img src="{{url('/images/w8.png')}}"></div>
-            <div class="title">Abibas Fusion</div>
-            <div class="desc">Skirt</div>
-            <div class="box">
-                <div class="price">RM 129.00</div>
-            </div>
-        </div>
-
+    @endforeach
     </div>
 </section>
 
 
 <div class="products-preview">
-    <div class="preview" data-target="p-1">
-        <img src="{{url('/images/w1.png')}}">
+    @foreach ($products as $product)
+    <div class="preview" data-target="{{$product->product_name}}">
+        <img src="{{$product->image}}">
         <table class="table">
 		<thead>
 			<tr>
@@ -89,10 +34,10 @@
 		</thead>
 		<tbody>
 			<tr>
-				<td><input type="checkbox" id="w1.s"><label for="w1.s">S</label></td>
-				<td><input type="checkbox" id="w1.m"><label for="w1.m">M</label></td>
-                <td><input type="checkbox" id="w1.l"><label for="w1.l">L</label></td>
-				<td><input type="checkbox" id="w1.xl"><label for="w1.xl">XL</label></td>
+				<td><input type="checkbox" id="m1.s"><label for="m1.s">S</label></td>
+				<td><input type="checkbox" id="m1.m"><label for="m1.m">M</label></td>
+                <td><input type="checkbox" id="m1.l"><label for="m1.l">L</label></td>
+				<td><input type="checkbox" id="m1.xl"><label for="m1.xl">XL</label></td>
             </tr>
         </tbody>
         </table>
@@ -105,198 +50,7 @@
             </div>
         </div>
     </div>
-
-    <div class="preview" data-target="p-2">
-        <img src="{{url('/images/w2.png')}}">
-        <table class="table">
-		<thead>
-			<tr>
-				<th colspan="8">Size Selection</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><input type="checkbox" id="w2.s"><label for="w2.s">S</label></td>
-				<td><input type="checkbox" id="w2.m"><label for="w2.m">M</label></td>
-                <td><input type="checkbox" id="w2.l"><label for="w2.l">L</label></td>
-				<td><input type="checkbox" id="w2.xl"><label for="w2.xl">XL</label></td>
-
-            </tr>
-        </tbody>
-        </table>
-        <div class="buttons">
-            <div class="btn1">
-                <a href="#" class="a">Add To Cart</a>
-            </div>
-            <div class="btn2">
-                <a href="#" class="c">Cancel</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="preview" data-target="p-3">
-        <img src="{{url('/images/w3.png')}}">
-        <table class="table">
-		<thead>
-			<tr>
-				<th colspan="8">Size Selection</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><input type="checkbox" id="w3.s"><label for="w3.s">S</label></td>
-				<td><input type="checkbox" id="w3.m"><label for="w3.m">M</label></td>
-                <td><input type="checkbox" id="w3.l"><label for="w3.l">L</label></td>
-				<td><input type="checkbox" id="w3.xl"><label for="w3.xl">XL</label></td>
-
-            </tr>
-        </tbody>
-        </table>
-        <div class="buttons">
-            <div class="btn1">
-                <a href="#" class="a">Add To Cart</a>
-            </div>
-            <div class="btn2">
-                <a href="#" class="c">Cancel</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="preview" data-target="p-4">
-        <img src="{{url('/images/w4.png')}}">
-        <table class="table">
-		<thead>
-			<tr>
-				<th colspan="8">Size Selection</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><input type="checkbox" id="w4.s"><label for="w4.s">S</label></td>
-				<td><input type="checkbox" id="w4.m"><label for="w4.m">M</label></td>
-                <td><input type="checkbox" id="w4.l"><label for="w4.l">L</label></td>
-				<td><input type="checkbox" id="w4.xl"><label for="w4.xl">XL</label></td>
-            </tr>
-        </tbody>
-        </table>
-        <div class="buttons">
-            <div class="btn1">
-                <a href="#" class="a">Add To Cart</a>
-            </div>
-            <div class="btn2">
-                <a href="#" class="c">Cancel</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="preview" data-target="p-5">
-        <img src="{{url('/images/w5.png')}}">
-        <table class="table">
-		<thead>
-			<tr>
-				<th colspan="8">Size Selection</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><input type="checkbox" id="w5.s"><label for="w5.s">S</label></td>
-				<td><input type="checkbox" id="w5.m"><label for="w5.m">M</label></td>
-                <td><input type="checkbox" id="w5.l"><label for="w5.l">L</label></td>
-				<td><input type="checkbox" id="w5.xl"><label for="w5.xl">XL</label></td>
-            </tr>
-        </tbody>
-        </table>
-        <div class="buttons">
-            <div class="btn1">
-                <a href="#" class="a">Add To Cart</a>
-            </div>
-            <div class="btn2">
-                <a href="#" class="c">Cancel</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="preview" data-target="p-6">
-        <img src="{{url('/images/w6.png')}}">
-        <table class="table">
-		<thead>
-			<tr>
-				<th colspan="8">Size Selection</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><input type="checkbox" id="w6.s"><label for="w6.s">S</label></td>
-				<td><input type="checkbox" id="w6.m"><label for="w6.m">M</label></td>
-                <td><input type="checkbox" id="w6.l"><label for="w6.l">L</label></td>
-				<td><input type="checkbox" id="w6.xl"><label for="w6.xl">XL</label></td>
-            </tr>
-        </tbody>
-        </table>
-        <div class="buttons">
-            <div class="btn1">
-                <a href="#" class="a">Add To Cart</a>
-            </div>
-            <div class="btn2">
-                <a href="#" class="c">Cancel</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="preview" data-target="p-7">
-        <img src="{{url('/images/w7.png')}}">
-        <table class="table">
-		<thead>
-			<tr>
-				<th colspan="8">Size Selection</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><input type="checkbox" id="w7.s"><label for="w7.s">S</label></td>
-				<td><input type="checkbox" id="w7.m"><label for="w7.m">M</label></td>
-                <td><input type="checkbox" id="w7.l"><label for="w7.l">L</label></td>
-				<td><input type="checkbox" id="w7.xl"><label for="w7.xl">XL</label></td>
-            </tr>
-        </tbody>
-        </table>
-        <div class="buttons">
-            <div class="btn1">
-                <a href="#" class="a">Add To Cart</a>
-            </div>
-            <div class="btn2">
-                <a href="#" class="c">Cancel</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="preview" data-target="p-8">
-        <img src="{{url('/images/w8.png')}}">
-        <table class="table">
-		<thead>
-			<tr>
-				<th colspan="8">Size Selection</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<td><input type="checkbox" id="w8.s"><label for="w8.s">S</label></td>
-				<td><input type="checkbox" id="w8.m"><label for="w8.m">M</label></td>
-                <td><input type="checkbox" id="w8.l"><label for="w8.l">L</label></td>
-				<td><input type="checkbox" id="w8.xl"><label for="w8.xl">XL</label></td>
-            </tr>
-        </tbody>
-        </table>
-        <div class="buttons">
-            <div class="btn1">
-                <a href="#" class="a">Add To Cart</a>
-            </div>
-            <div class="btn2">
-                <a href="#" class="c">Cancel</a>
-            </div>
-        </div>
-    </div>
-
+    @endforeach
 </div>
 
 <script>
