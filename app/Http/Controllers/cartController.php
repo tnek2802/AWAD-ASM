189553f60182f8afc6e07019e73fb4e8a54303c2
jasperyin1234 <jasperyin1234@gmail.com>
2023-04-10 01:53:34 +0800

    Checking size amount

--- app/Http/Controllers/cartController.php
+++ app/Http/Controllers/cartController.php
@@ -22,40 +22,70 @@ class cartController extends Controller
     public function addItem(Request $request)
     {
         $product_id = $request->input('product_id');
-        $size = $request->input('size');
+        $size = $request->input('size'); // S M L XL
+        $productKey = $product_id . $size;
         $product = Product::find($product_id);
-        dd($product);
+
+        if ($product->product_category == "Clothes")
+            $stock = ClothesSize::where('product_id', $product_id)->value($size);
+        else
+            $stock = ShoeSize::where('product_id', $product_id)->value($size);
+
         $cart = session()->get('cart');
-        
+
+        // FIRST CONDITION -> Cart session not set
         if (!$cart) {
+
+            if ($stock <= 0) {
+                dd('OUT OF STOCK! 1');
+                return redirect()->back()->with('failure', 'Product is out of stock!');
+            }
+
             $cart = [
-                $product_id => [
+                $productKey => [
+                    "product_id" => $product->product_id,
                     "product_name" => $product->product_name,
                     "quantity" => 1,
                     "product_price" => $product->product_price,
                     "size" => $size,
                 ]
             ];
+
+            session()->put('cart', $cart);
+            return redirect()->back()->with('success', 'Product added to cart successfully!');
+        }
+
+        // SECOND CONDITION -> Cart & Product alrdy added 
+        if (isset($cart[$productKey])) {
             
+            $cart[$productKey]['quantity']++; 
+
+            if ($stock < $cart[$productKey]['quantity']) { 
+                return redirect()->back()->with('failure', 'Product is out of stock!');
+            }
+
             session()->put('cart', $cart);
-            dd($cart);
+            // dd($cart);
             return redirect()->back()->with('success', 'Product added to cart successfully!');
         }
 
-        if (isset($cart[$product_id][$size])) {
-            $cart[$product_id][$size]['quantity']++;
+        // THIRD CONDITION -> Cart existed but Product not added yet
+        else {
 
-            session()->put('cart', $cart);
-            dd($cart);
-            return redirect()->back()->with('success', 'Product added to cart successfully!');
+            if ($stock <= 0) {
+                dd('OUT OF STOCK! 3');
+                return redirect()->back()->with('failure', 'Product is out of stock!');
+            }
+
+            $cart[$productKey] = [
+                "product_id" => $product->product_id,
+                "product_name" => $product->name,
+                "quantity" => 1,
+                "product_price" => $product->price,
+                "size" => $size,
+            ];
+            
         }
-
-        $cart[$product_id] = [
-            "product_name" => $product->name,
-            "quantity" => 1,
-            "product_price" => $product->price,
-            "size" => $size,
-        ];
 
         session()->put('cart', $cart);
         dd($cart);
