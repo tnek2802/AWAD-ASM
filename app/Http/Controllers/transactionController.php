b170a757470f034cabae9e6e099a7605331e8906
yenkhai8 <limyenjhai01@gmail.com>
2023-04-10 01:22:31 +0800

    asda

--- app/Http/Controllers/transactionController.php
+++ app/Http/Controllers/transactionController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Support\Facades\DB; // import database
 use App\Models\Transaction; // import transaction 
+use App\Models\Product; // import model 
 
 class transactionController extends Controller
 {
@@ -26,6 +27,5 @@ class transactionController extends Controller
             'orders' => $orders,
             'products' => $products,
         ]);
-        // return view('orderDetails');
     }
 }
