3a893d739b2f2c09263794d21bfe7c8aa5f5194f
YenKhai <intern2@dsm.com.my>
2023-04-09 16:40:25 +0800

    create branch, create css create view

---
+++ resources/views/orderDetails.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+@section('content')
+<html>
+    <head>
+        <link rel="stylesheet" href="{{url('css/orderDetails.css')}}"/>
+    </head>
+</html>
+
+
+
+
+
+
+@endsection
