46427369b2d5a2df5fdafc4aa24ed2e4f62b18fd
YenKhai <intern2@dsm.com.my>
2023-04-09 19:39:09 +0800

    done

--- app/Http/Controllers/transactionController.php
+++ app/Http/Controllers/transactionController.php
@@ -9,37 +9,23 @@ use App\Models\Product; // import model
 
 class transactionController extends Controller
 {
-    // public function getOrder(Request $req)
-    // {
-    //     $transaction_id = Transaction::find($req->user_id);
-    //     foreach ($transaction_id->products as $product_id) {
-    //         $roles = Transaction::find(1)->roles()->orderBy('name')->get();
-    //     }
-    //     $products = Product::find($req->user_id);
-    //     return view('home', ['products' => $products]);
-    // }
-
     public function getOrderDetails($id)
     {
-        // $transactions = Transaction::where('user_id', $id)->get();
-        // $products = collect();
-        // foreach ($transactions as $transaction) {
-        //     $products = $products->merge($transaction->Products);
-        // }
-        // return view('orderDetails', ['products' => $products]);
-
-        $orders = Transaction::where('user_id', $id)->get();
-        $transactions = collect();
+        $transactions = Transaction::where('user_id', $id)->get();
+        $orders = collect();
         $products = collect();
-        foreach ($orders as $order) {
-            $transactions->push($order);
-            $products = $products->merge($order->Products);
+
+        foreach ($transactions as $transaction) {
+            $orders->push($transaction);
+            $product_transaction = $transaction->Products;
+            foreach ($product_transaction as $product) {
+                $product->transaction_id = $transaction->id;
+                $products->push($product);
+            }
         }
         return view('orderDetails', [
-            'transactions' => $transactions,
+            'orders' => $orders,
             'products' => $products,
         ]);
-
     }
-
 }
--- resources/views/orderDetails.blade.php
+++ resources/views/orderDetails.blade.php
@@ -10,9 +10,14 @@
 
     <div class=test>
         <h1>hey there here are your orders xd</h1>
-        @foreach ($products as $product)
+        {{-- @foreach($orders as $order)
+        <div>{{$order->transaction_id}}</div>
+        @endforeach --}}
+
+        {{-- @foreach ($products as $product)
         <div class="col-md-3">
             <div class="card">
+                <div class="card-header">Order ID:{{ $product->transaction_id}}</div>
                 <div class="card-header">{{ $product->product_name }}</div>
                 <div class="card-body">
                     <img src="{{ $product->image }}" class='img-fluid'>
@@ -21,6 +26,30 @@
                 </div>
             </div>
         </div>
-        @endforeach
+        @endforeach --}}
+
+        @foreach ($orders as $order)
+    <div class="card mb-3">
+        <div class="card-header">
+            Order ID: {{ $order->transaction_id }}
+        </div>
+        <div class="card-body">
+            @foreach ($order->Products as $product)
+                <div class="media mb-3">
+                    <img src="{{ $product->image }}" class="mr-3" alt="{{ $product->product_name }}" width="64">
+                    <div class="media-body">
+                        <h5 class="mt-0">{{ $product->product_name }}</h5>
+                        <p>{{ $product->product_description }}</p>
+                        <p>Price: {{ $product->product_price }}</p>
+                        <p>Quantity: {{ $product->pivot->transaction_quantity }}</p>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        <div class="card-footer">
+            <strong>Total:</strong> {{ $order->total_amount }}
+        </div>
+    </div>
+@endforeach
     </div>
 @endsection
