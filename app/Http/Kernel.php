8561a5e2a0b168c50f9dc93b01dd7ecf1145fe72
Tnek Shiin Wei <tnekshiinwei@gmail.com>
2023-04-11 16:46:06 +0800

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,7 +57,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'protectedPage' => \App\Http\Middleware\adminCheck::class,
-        'protectedCart' => \App\Http\Middleware\CartMiddleware::class,
+        'protectedCart' => \App\Http\Middleware\cartCheck::class,
         'protectedOrders' => \App\Http\Middleware\ownOrder::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
